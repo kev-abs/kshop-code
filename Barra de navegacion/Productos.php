3d41528bb7df89b09555a2875404b02f26d7456d
user.email <manuel25142808@gmail.com>
2025-07-03 14:52:47 -0500

    Update Productos.php

--- Barra de navegacion/Productos.php
+++ Barra de navegacion/Productos.php
@@ -45,7 +45,7 @@ $productos = [
   </form>
   <nav class="navbar">
     <ul>
-      <li><a href="index.php">Inicio</a></li>
+      <li><a href="../index.php">Inicio</a></li>
       <li><a href="Productos.php">Productos</a></li>
       <li><a href="servicios.php">Servicios</a></li>
       <li><a href="contactos.php">Contáctanos</a></li>
