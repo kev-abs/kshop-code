93d5bcc8dd4fe80f9e851a706563f03f1cc1a667
user.email <manuel25142808@gmail.com>
2025-07-07 08:40:55 -0500

    Update paneladmin.php

--- Paneles/paneladmin.php
+++ Paneles/paneladmin.php
@@ -5,7 +5,6 @@ session_start();
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
 if (!isset($_SESSION["rol"]) || $_SESSION["rol"] !== "administrador") {
     header("Location: ../Barra de navegacion/Iniciarsesion.php");
