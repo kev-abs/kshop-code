78173bdf6bf20e38f920bf1a7acb89ccd4cc17cc
user.email <manuel25142808@gmail.com>
2025-07-07 07:04:19 -0500

    Update cerrarsesion.php

--- php/cerrarsesion.php
+++ php/cerrarsesion.php
@@ -3,11 +3,10 @@ session_start();
 session_unset();    // Limpia todas las variables de sesión
 session_destroy();  // Destruye la sesión
 
-// Evitar que el navegador guarde en caché esta página
+// Evita que el navegador guarde la página anterior en caché
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+header("Expires: 0");
 
 // Redirige al login
 header("Location: ../Barra de navegacion/Iniciarsesion.php");
