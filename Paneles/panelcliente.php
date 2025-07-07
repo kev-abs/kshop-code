03ac78174d8ea3960cd562d1189ee1c34e1b8b53
user.email <manuel25142808@gmail.com>
2025-07-07 07:04:08 -0500

    Update panelcliente.php

--- Paneles/panelcliente.php
+++ Paneles/panelcliente.php
@@ -1,15 +1,9 @@
 <?php
-// Evitar que el navegador guarde en caché esta página
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 session_start();
 if (!isset($_SESSION["rol"]) || $_SESSION["rol"] !== "cliente") {
     header("Location: ../Barra de navegacion/Iniciarsesion.php");
     exit();
 }
-
 
 $productos = [
   [
